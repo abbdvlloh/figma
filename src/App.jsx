9a007895169ffe74e696ac9790465879f8a5bcec
abbdvlloh <uldansulajmanova683@example.com>
2024-11-11 23:51:00 +0500

    navbar bor

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Typography, Card } from "@material-tailwind/react";
+
+export default function App() {
+  return (
+    <>
+    <h1>navbar</h1>
+   <h1>Lorem ipsum dolor sit.</h1>
+   </>
+    );
+}
